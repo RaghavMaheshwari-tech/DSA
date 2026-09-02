663d56344571536bd1f20fb675b613aca5ae1b7d
Raghav Maheshwari <raghavmalpani62@gmail.com>
2026-09-02 20:49:52 +0530

    Time: 12 ms (70.38%) | Memory: 36.9 MB (45.34%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    unordered_map<int,vector<int>>mp;//number,columns
-    int n;
-    int ans=0;
-
-    void solve(int row, vector<int>arr, vector<vector<int>> &grid){
-
-        for(int i=0;i<arr.size();i++){//itne sab mai (row==col) element
-            bool flag = 1;
-            for(int j=0;j<n;j++){
-                if(grid[row][j]!=grid[j][arr[i]]){
-                    flag=0;
-                    break;
-                }
-            }
-            if(flag) ans++;
-        }
-
-        return ;
-    }
-
     int equalPairs(vector<vector<int>>& grid) {
-        n = grid.size();
+        int n = grid.size();
+        int ans = 0;
         
-        for(int i=0;i<n;i++){
-            mp[grid[0][i]].push_back(i);
+        // Map to store frequency of each row
+        map<vector<int>, int> rowCount;
+        for(int i = 0; i < n; i++) {
+            rowCount[grid[i]]++; 
         }
 
-        for(int i=0;i<n;i++){
-            if(mp.find(grid[i][0])!=mp.end()){
-                solve(i,mp[grid[i][0]], grid);
+        // Check each column
+        for(int j = 0; j < n; j++) {
+            vector<int> col;
+            for(int i = 0; i < n; i++) {
+                col.push_back(grid[i][j]); // Extracting column elements
             }
+            // Agar yeh column kisi row jaisa hai, toh uski frequency add kar do
+            ans += rowCount[col];
         }
 
         return ans;
