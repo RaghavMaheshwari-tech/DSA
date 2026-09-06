41a46a0a0f5cc186454c4bb07d3c14ff285d4b8c
Raghav Maheshwari <raghavmalpani62@gmail.com>
2026-09-06 16:28:08 +0530

    Time: 3 ms (64.04%) | Memory: 10.7 MB (88.06%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -2,36 +2,39 @@ class Solution {
 public:
     string simplifyPath(string path) {
         vector<string>st;
-        string temp;
+
         int n = path.size();
+        string temp;
 
         for(int i=0;i<=n;i++){
 
-            if(i==n || path[i]=='/'){
-
-                if(temp=="" || temp=="."){
+            if(path[i]=='/' || i==n){
+                if(temp=="." || temp=="") {
 
                 }
 
                 else if(temp==".."){
-                    if(!st.empty()) st.pop_back();
-                } 
+                    if(st.size()>0){
+                        st.pop_back();
+                    }
+                }
 
                 else st.push_back(temp);
 
                 temp = "";
             }
+
             else temp.push_back(path[i]);
         }
-
+        
         string ans;
 
-        for(auto word:st){
-            ans += "/"+ word;
+
+        for(auto word: st){
+            ans+= '/'+word;
         }
 
         if(ans.size()==0) return "/";
-
-        return ans;
+        else return ans;
     }
 };
