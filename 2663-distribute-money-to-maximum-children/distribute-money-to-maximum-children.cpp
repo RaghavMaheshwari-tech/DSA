97243673f2673db77b2cab4f5b236f67e19b2589
Raghav Maheshwari <raghavmalpani62@gmail.com>
2026-09-08 15:48:02 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (33.81%) - LeetSync

--- 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.cpp
+++ 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int distMoney(int money, int children) {
-        if(money<children) return -1;
-        else{
-            int y = money-children;//sbko 1 dediya
-            int x = y/7;//children who get exactly 8 after accepting 7
-            int z = y%7;//left money
-            if(x==0) return 0;//8 bn hi nhi skta
-            else if(x>children) return children-1;//8 wale count jada ho gye
-            else if(x==children){
-                if(z>0) return x-1;
-            }
-            //kuch hi 8 bnege 
-            int left = children-x;
-            int left_money = y-x*7;
-            if(left==1 && left_money==3) return x-1;
-            else return x;
-        }
-
-        return -1;
+        money -= children;
+        if (money < 0)
+            return -1;
+        if (money / 7 == children && money % 7 == 0)
+            return children;
+        if (money / 7 == children - 1 && money % 7 == 3)
+            return children - 2;
+        return min(children - 1, money / 7);
     }
 };
