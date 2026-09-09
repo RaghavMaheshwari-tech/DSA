10b710875da93a8d88b86e9d813f7b34f8138e57
Raghav Maheshwari <raghavmalpani62@gmail.com>
2026-09-09 17:44:10 +0530

    Time: 15 ms (17.54%) | Memory: 14 MB (73.74%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int>st;
+        int n = s.size();
+
+        for(int i=0;i<n;i++){
+            if(s[i]!=')' && s[i]!='(') continue;
+
+            else if(s[i]=='(') st.push(i); 
+            else if(st.empty() && s[i]==')') s[i] = '#';
+            else st.pop(); 
+        }
+
+        while(!st.empty()){
+            s[st.top()]='#';
+            st.pop();
+        }
+
+        string ans;
+        for(int i=0;i<n;i++){
+            if(s[i]!='#') ans.push_back(s[i]);
+        }
+
+
+        return ans;
+    }
+};
